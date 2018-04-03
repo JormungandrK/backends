2e3e5fb0bdda031ef7b48838c3cc88b0d2594d34
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-04-03 16:35:56 +0200

    Unit tests for validation.

--- validation_test.go
+++ validation_test.go
@@ -123,3 +123,113 @@ func TestValidateBackendVanilla(t *testing.T) {
 		t.Fatal(fmt.Sprintf("Validation errors:\n%s", strings.Join(result.Errors, "\n")))
 	}
 }
+
+func TestValidationMissingRequiredValues(t *testing.T) {
+	result, err := ValidateBackend(map[string]interface{}{
+		//"host":     "192.168.1.90:89-9",
+		"database": "users",
+		"type":     "mongodb",
+		"credentials": map[string]interface{}{
+			"username": "test",
+			"password": "pass",
+		},
+		"collections": map[string]interface{}{
+			"users": map[string]interface{}{
+				"name": "users",
+				"indexes": []map[string]interface{}{
+					map[string]interface{}{
+						"columns": []string{"id"},
+						"unique":  true,
+					},
+					map[string]interface{}{
+						"columns": []string{"email"},
+						"unique":  true,
+					},
+				},
+			},
+			"tokens": map[string]interface{}{
+				"name": "tokens",
+				"indexes": []map[string]interface{}{
+					map[string]interface{}{
+						"columns": []string{"id"},
+						"unique":  true,
+					},
+					map[string]interface{}{
+						"columns": []string{"token"},
+						"unique":  true,
+					},
+				},
+				"enableTtl":    true,
+				"ttl":          3600000,
+				"ttlAttribute": "createdAt",
+			},
+		},
+	}, backendSchema)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.Valid {
+		t.Fatal("Expected validation to fail.")
+	}
+	if len(result.Errors) == 0 {
+		t.Fatal("Expected error messages to be returned in the validation result.")
+	}
+	if !strings.Contains(result.Errors[0], "host") {
+		t.Fatalf("Expected validation message to contain the property 'host'. Instead the error message is: %s", result.Errors[0])
+	}
+}
+
+func TestValidateBackendIncorrectType(t *testing.T) {
+	result, err := ValidateBackend(map[string]interface{}{
+		"host":     true,
+		"database": "users",
+		"type":     "mongodb",
+		"credentials": map[string]interface{}{
+			"username": "test",
+			"password": "pass",
+		},
+		"collections": map[string]interface{}{
+			"users": map[string]interface{}{
+				"name": "users",
+				"indexes": []map[string]interface{}{
+					map[string]interface{}{
+						"columns": []string{"id"},
+						"unique":  true,
+					},
+					map[string]interface{}{
+						"columns": []string{"email"},
+						"unique":  true,
+					},
+				},
+			},
+			"tokens": map[string]interface{}{
+				"name": "tokens",
+				"indexes": []map[string]interface{}{
+					map[string]interface{}{
+						"columns": []string{"id"},
+						"unique":  true,
+					},
+					map[string]interface{}{
+						"columns": []string{"token"},
+						"unique":  true,
+					},
+				},
+				"enableTtl":    true,
+				"ttl":          3600000,
+				"ttlAttribute": "createdAt",
+			},
+		},
+	}, backendSchema)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.Valid {
+		t.Fatal("Expected validation to fail.")
+	}
+	if result.Errors == nil || len(result.Errors) == 0 {
+		t.Fatal("Expected to get error messages")
+	}
+	if !strings.Contains(result.Errors[0], "host") || !strings.Contains(result.Errors[0], "type") {
+		t.Fatalf("Expected the error message to contain the property 'host' and to reference the invalid type. Instead the message is: %s", result.Errors[0])
+	}
+}
